45439fbd7fd3fd91bd13b49c31abcf1a751e8b7b
Javier De la Hoz <javierdlahoz@gmail.com>
2020-04-12 11:59:34 +0200

    splash screen and spin and win

---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="splash-screen">
+        <img v-bind:src="logoSrc">
+        <div class="progress mt-4">
+            <div class="progress-bar" v-bind:style="progressClass"
+                 role="progressbar" v-bind:aria-valuenow="loadingPercentage"
+                 aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            loadingPercentage: Number
+        },
+        computed: {
+            logoSrc() {
+                return require(`../assets/themes/${window.slotConfig.theme}/logo.png`);
+            },
+            progressClass() {
+                return `width: ${this.loadingPercentage}%`;
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ Vue.component('slot-control-panel',  require('./components/ControlPanel').defaul
 Vue.component('slot-wrapper', require('./components/Wrapper').default);
 Vue.component('slot-reel', require('./components/Reel').default);
 Vue.component('slot-symbol', require('./components/Symbol').default);
+Vue.component('splash-screen', require('./components/SplashScreen').default);
 
 new Vue({
   store,
