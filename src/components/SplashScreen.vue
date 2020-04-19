b62b6964b1357a52ea6015c45f5447dd8c48fde8
Javier De la Hoz <javierdlahoz@gmail.com>
2020-04-19 14:11:24 +0200

    send bearer token in request headers

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="splash-screen">
-        <img v-bind:src="logoSrc">
-        <div class="spinner mt-4">
-            <div class="fa-3x text-white">
-                <font-awesome-icon icon="spinner" pulse />
-            </div>
-<!--            <div class="progress-bar" v-bind:style="progressClass"-->
-<!--                 role="progressbar" v-bind:aria-valuenow="loadingPercentage"-->
-<!--                 aria-valuemin="0" aria-valuemax="100"></div>-->
-        </div>
+  <div class="splash-screen">
+    <img v-bind:src="logoSrc">
+    <div class="spinner mt-4">
+      <div class="fa-3x text-white">
+        <font-awesome-icon icon="spinner" pulse/>
+      </div>
+      <!--            <div class="progress-bar" v-bind:style="progressClass"-->
+      <!--                 role="progressbar" v-bind:aria-valuenow="loadingPercentage"-->
+      <!--                 aria-valuemin="0" aria-valuemax="100"></div>-->
     </div>
+  </div>
 </template>
 <script>
-    export default {
-        props: {
-            loadingPercentage: Number
-        },
-        computed: {
-            logoSrc() {
-                return require(`../assets/themes/${window.slotConfig.theme}/logo.png`);
-            },
-            progressClass() {
-                return `width: ${this.loadingPercentage}%`;
-            }
-        }
+  export default {
+    props: {
+      loadingPercentage: Number
+    },
+    computed: {
+      logoSrc() {
+        return require(`../assets/themes/${window.slotConfig.theme}/logo.png`);
+      },
+      progressClass() {
+        return `width: ${this.loadingPercentage}%`;
+      }
     }
+  }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    bearerToken: null
   },
   mutations: {
+    SET_BEARER_TOKEN(state, token) {
+      state.bearerToken = token;
+    }
   },
   actions: {
+    setBearerToken: ({ commit }, token) => {
+      commit('SET_BEARER_TOKEN', token);
+    }
   },
-  modules: {
+  modules: {},
+  getters: {
+    bearerToken: state => {
+      return state.bearerToken;
+    }
   }
 })
