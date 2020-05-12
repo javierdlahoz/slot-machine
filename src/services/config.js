86b3b9e2aaa78875e33dea5b660cacddd75ece1d
Javier de la Hoz <javierdlahoz@gmail.com>
2020-05-12 09:27:23 +0200

    improve experience on 5 cols

--- src/services/config.js
+++ src/services/config.js
@@ -3,9 +3,9 @@ const config = {
   reels: 3,
   rows: 1,
   cols: 3,
-  animationDuration: 4000,
+  animationDuration: 3000,
   animationSpeed: 9000, // the higher the number the faster
-  stopDelay: 150,
+  stopDelay: 300,
   gameId: 1,
   baseUrl: `https://casino.nomadnt.com`,
   token: `very-strong-random-string-1`
